5a58e60d1dd68336636df65d78720a4a846f17be
EddyBalan <vic.balan.27@live.com>
2020-02-09 21:15:39 -0600

    changes to the whole app

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,10 @@
     <!-- navBar -->
     <div>
       <v-app-bar
-        color="pink accent-1 white--text"
+        color="cyan accent-3 white--text"
       >
-        <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-        <v-toolbar-title>Kenia</v-toolbar-title>
+        <v-toolbar-title>Simple Web-app</v-toolbar-title>
 
       </v-app-bar>
     </div>
@@ -15,11 +14,11 @@
     <div class="ma-6 mt-12" id="greetingCard">
       <v-card width="400" class="mx-auto mt-12">
         <v-card-title>
-          <h3 v-if = "isMorning()" class="mx-auto font-weight-regular">Buenos dias</h3>
+          <h3 v-if = "isMorning()" class="mx-auto font-weight-regular">Rise and shine, lazy bones!</h3>
 
-          <h3 v-else-if = "isAfternoon()"  class="mx-auto font-weight-regular">Buenas tardes</h3>
+          <h3 v-else-if = "isAfternoon()"  class="mx-auto font-weight-regular">Goodevening chaps!</h3>
 
-          <h3 v-else-if = "isNight()" class="mx-auto font-weight-regular">Buenas noches</h3>
+          <h3 v-else-if = "isNight()" class="mx-auto font-weight-regular">Boa Noite!</h3>
         </v-card-title>
 
         <v-card-title>
@@ -27,7 +26,12 @@
         </v-card-title>
         
         <v-card-actions>
-                <v-btn class="mx-auto" v-on:click= "alert1" color="pink lighten-1 white--text font-weight-regular">Click me!</v-btn>
+                <v-btn 
+                class="mx-auto" 
+                v-on:click= "alert1" 
+                color="cyan accent-3 white--text font-weight-regular">
+                  Click me!
+                </v-btn>
         </v-card-actions>
       </v-card>
     </div>
@@ -35,16 +39,22 @@
     <!-- NameGame -->
     <div class="ma-6 mt-n4">
       <v-card width="400" class="mx-auto mt-3">
+
         <v-card-title>
-          <h3 class="mx-auto font-weight-regular">Los nombres de Kenia</h3>
+          <h3 class="mx-auto font-weight-regular">What's a VENM Stack?</h3>
         </v-card-title>
         
-        <v-card-title>
-            <p class="mx-auto font-weight-regular">{{ chosenName }}</p>
-        </v-card-title>
+            <v-card-title>
+                <p class="mx-auto font-weight-regular">{{ chosenName }}</p>
+            </v-card-title>
 
         <v-card-actions>
-          <v-btn class="mx-auto" @click= "picker" color="pink lighten-1 white--text font-weight-regular">Tus Nombres</v-btn>
+          <v-btn 
+          class="mx-auto" 
+          @click= "picker" 
+          color="cyan accent-3 white--text font-weight-regular">
+          VENM
+          </v-btn>
         </v-card-actions>
       </v-card>
     </div>
@@ -57,13 +67,15 @@
     max-width="400"
   >
     <v-card-title>
-      <v-icon
-        large
-        left
-      >
-        mdi-twitter
-      </v-icon>
-      <span class="title font-weight-light">Twitter</span>
+      <v-list-item-avatar
+       color="grey darken-3"
+       >
+          <v-img
+            class="elevation-6"
+            src="../public/eddy-twitter.jpg"
+          ></v-img>
+        </v-list-item-avatar>
+      <span class="title font-weight-light">@SandwichPirate_</span>
     </v-card-title>
 
     <v-card-text class="headline font-weight-bold">
@@ -72,16 +84,6 @@
 
     <v-card-actions>
       <v-list-item class="grow">
-        <v-list-item-avatar color="grey darken-3">
-          <v-img
-            class="elevation-6"
-            src="../public/eddy-twitter.jpg"
-          ></v-img>
-        </v-list-item-avatar>
-
-        <v-list-item-content>
-          <v-list-item-title>@SandwichPirate_</v-list-item-title>
-        </v-list-item-content>
 
         <v-row
           align="center"
@@ -95,10 +97,11 @@
         </v-row>
       </v-list-item>
     </v-card-actions>
+
   </v-card>
   </div>
   <!-- PictureCards -->
-    <div class="ma-12 mt-n4">
+    <div class="ma-12 mt-n1">
      <v-card
     class="mx-auto"
     max-width="500"
@@ -145,17 +148,14 @@ export default {
   components: {},
 
   data: () => ({
-        day1: 'Dia uno 😥 ',
-        day2: 'Dia dos, aun no me acostumbro 😟',
-        day3: 'Dia tres, I miss you 🥺',
-        day4: 'Dia cuatro, cuan aburrido es estar sin ti 😒',
-        day5: 'Dia cinco, ya mero regresas!😏',
-        list: ['Mi amor','Cosita', 'Cosa bella','Hermosa','Preciosa','Princesa','Chiky 🍪','Mi ardilla 🐿','Corazon','Baby','Mi vida','Mi mejor amiga','Mi Cielo','Mujer mas hermosa del mundo','Mi futuro','Mi Felicidad','Flaka','Ojos de miel','Mi enojona','Perfecta','Mi corazon de melon'],
+
+        list: ['Vue Express Node.js MongoDB', 'Victor Exports Number Modules', 'Vanessa Enjoys Negotiating Money'],
         chosenName: '',
         cards: [
-        { title: '', src: 'https://lh3.googleusercontent.com/Ri8eD5C-v6DyUW3TyD6Ajp4gkyyqUbuIx_hQvuOS0-9uZPRZiqZFhBwh625qsDX3_Rm8HPmPIfnUqWLOiCMEGAIV34OwBqRCp0T3KQKIMBp93_J_bsl5R5iinCdB8EWxYxJKb1iZ3XjD1urv8o8R7zRT-iq3PQAandcP5c6DUUjGtsOkdyQkCISV2zbRF4lT9zpaGN43sqpTPaVT3qyLPIpi3URTldTF0o7Gw5Y2dGQIBSSILhgKWTR-wF8368YQcwXe1GyauNInDevQTywVyjfBl48BOAAFde8p-wdx4MKzlKCNXzpvQG2OGZcM76XjYgg41CK0UxtOx9uYooADCt1ThAvXtVcOrZfnbRxBoj9E5hG-8ix1EpWsFzPqjz-qCMHmAb6bf7_nr9wZ30dWlIIZ5CRj5y70-vAPMUrcvY0y9TLfdwOgvdgnLnoOUnrUJP7iCJMJ6vNJoeRGYjZG7lL7dOJQjhgA-pWR23FjIytB-xuLlVUddHlBOGI2O7KcMrwgzyLpMRG8j9aAb6TGXvFAyHtEQ1JdNKxpd3btUjo4luhIev1TwiA1Nj80dc9XZlNhoupZwIsBmDURDqUpZ5i_WfVSQ4Yxy4t9PXoeefy33frgRYgmbwdWrugo3F9gxSuYGhO4BhdpxxyKyQeN2ldvbhT-vt_eMYAV3jVbOyWyu8QgtgdkbCI=w375-h500-no', flex: 12 },
+        { title: 'Rambo the Mangy mutt', src: 'https://lh3.googleusercontent.com/Nd_4xnQiWDs_H-wX0cK797PvDbcSK1V5S_wYGruB5WnRvp_d1qmtFj3NUW3iI2HuZxII2QSgp8q8gS8vauHaq9ckt6IvoTiLVMgBNK6vNTxHS2G3bR21CLadzbkkTUfJGZVMD2RVTfDEjgwgHZOpdriSZJ9pQIyhB1mHQDyFtF1B0kMN8iMOqIuApW4Pohh4t6aXj0k5SN4-yHF3sjkVFXb-6u5Xxk8iO-C7lWq9TeYc5estXB-G3XrzMRqoNLOz_n-LBtRVxKwK7VSFLIAV_s7biC_3XDtIYRujbQPvaRxPmb4GqIgi8EfbXNjCuV8Jmgaq1xtKvGLWWA401eUZ-YL2qlYF-bCKksidz7Jpn0THnzs_Hn7Ky_m99rQG7PvZDgcjPnDdH_7AYkzumSsDwpwFyYKjU3MHX2BG5RxHlIj8uJIsvRjeRU-SsonblyN7FzyBzvfKFJ3e0eMZyi-rZ_Bt0T2MQavUtMWsDMkdh0yuVc9_EmJQc_CZ-pGcrJZXCYjyUXbTTcg_O_7GnSxFbfxT5MRUUWANPKPxvQxH0U-Xyf1clCdp1RyrejqSd4dQhHKPwQIV0t7bZa-8W9oI2FI4KC_ENrrrmtZc-Id1KX0IqtEmopPOvN10ddMz-r13P9j2TxkkiWyS5om_LW9QiXI1UYbD91OmW_Ll43DWvjbUQMqj0AJwIMs=w756-h1007-no', flex: 12 },
         ],
         like1: 0,
+        
   }),
   methods: {
         isMorning() {
@@ -170,13 +170,9 @@ export default {
 
         alert1: function () {
             function greet1() {
-                alert('')
+                alert('Where are you going? Stay here!')
             }
-            function redirect1() {
-                window.location.href = "https://www.youtube.com/watch?v=-2n03Ufzni8";
-            }
-            greet1()
-            redirect1()
+              greet1()
         },
         picker: function() {
             var chosenNumber = Math.floor(Math.random() * this.list.length)
